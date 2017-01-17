40fba493aa7a99ac051de59b3db593126de72eb0
vinaynadagoud <nadagoud.v@husky.neu.edu>
2017-01-17 02:57:33 -0500

    css modifications

--- web-dev-vin/test/app.js
+++ web-dev-vin/test/app.js
@@ -4,8 +4,8 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    //var connectionString = 'mongodb://127.0.0.1:27017/test';
-    var connectionString = 'mongodb://vinay:cs5610@ds111549.mlab.com:11549/webdevcs5610'
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    //var connectionString = 'mongodb://vinay:cs5610@ds111549.mlab.com:11549/webdevcs5610'
 
     if(process.env.MLAB_USERNAME) {
         connectionString = process.env.MLAB_USERNAME + ":" +
